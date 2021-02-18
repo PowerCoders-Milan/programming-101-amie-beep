fc2ced1afc7fb20e4559bd79819d34470c286d15
amie senghore <amiesenghore1@gmail.com>
2021-02-18 14:06:47 +0100

    exercise1

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,12 @@
 //Insert level 9 code below
+moveForward();
+turnLeft();
+while (notDone()) {
+  if (isPathForward()) {
+    moveForward();
+  } else {
+    if (isPathLeft()) {
+      turnLeft();
+    }
+  }
+}
